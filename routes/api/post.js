8488f5c3aaa73081864bd16efee5133dcd9cd550
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-05-15 23:18:05 -0500

    get post by id

--- routes/api/post.js
+++ routes/api/post.js
@@ -41,4 +41,25 @@ router.get("/", auth, async(req, res) =>{
   }
 })
 
+// @route Get api/posts/:id
+// @desc Get post by id
+// @access Private
+router.get("/:id", auth, async(req,res)=>{
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+      return res.status(404).send("Post not found")
+    }
+
+    res.json(post)
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).send("Post not found");
+    }
+    res.status(500).send("Server Error");
+  }
+})
+
 module.exports = router
