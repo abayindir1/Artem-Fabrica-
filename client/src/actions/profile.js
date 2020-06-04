f8812303f3e4bea0c4f4c27f4273484c625f29e4
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-06-04 17:00:04 -0500

    profile reducer and action

---
+++ client/src/actions/profile.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { PROFILE_ERROR, GET_PROFILE } from "./types";
+import { setAlert } from "./alert";
+
+// Get Current user's profile
+export const getCurrentProfile = () => async dispatch =>{
+    
+    try {
+        const res = axios.get("/api/profile/me")
+
+    dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+    })
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: error.response.statusText, status: error.response.status}
+        })
+    }
+}
