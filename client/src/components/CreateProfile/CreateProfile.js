9397fa6df8b87642a2c3016918608cc4dd99d157
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-06-05 13:10:40 -0500

    update form, add select states

--- client/src/components/CreateProfile/CreateProfile.js
+++ client/src/components/CreateProfile/CreateProfile.js
@@ -1,37 +1,36 @@
-import React, {Fragment, useState} from 'react'
-import PropTypes from 'prop-types'
-import {connect} from "react-redux"
-import { body } from 'express-validator'
+import React, { Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { body } from "express-validator";
 
-const CreateProfile = props => {
-    const[formData, setFormData] = React.useState({
-        bio: "",
-        location: "",
-        facebook: "",
-        instagram: "",
-        twitter:""
-    }) 
+const CreateProfile = (props) => {
+  const [formData, setFormData] = React.useState({
+    bio: "",
+    location: "",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  });
 
-    const {bio, location, facebook, instagram, twitter} = formData
+  const { bio, location, facebook, instagram, twitter } = formData;
 
-    const onChange = (e) =>{
-        setFormData({...formData, [e.target.name]: e.target.value})
-    }
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const onSubmit = async (e) =>{
-        e.preventDefault()
-        console.log(formData)
-    }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
-
-    return (
-        <Fragment>
+  return (
+    <Fragment>
       <div className="sign-in-form">
-        <h1 className="lead">
-          <i className="fas fa-user"></i> Create A Profile
-        </h1>
+        <h2>
+          <i class="fas fa-id-card"></i> Create A Profile
+        </h2>
         <form className="form" onSubmit={(e) => onSubmit(e)}>
-        <div className="form-group">
+          <div className="form-group">
             <input
               type="bio"
               placeholder="Bio"
@@ -42,48 +41,106 @@ const CreateProfile = props => {
             />
           </div>
           <div className="form-group">
-            <input
+            <select
               type="location"
               placeholder="Your Location (Optional)"
               name="location"
               value={location}
               onChange={(e) => onChange(e)}
-            />
-          </div><div className="form-group">
+            >
+              <option value="AL">Alabama</option>
+              <option value="AK">Alaska</option>
+              <option value="AZ">Arizona</option>
+              <option value="AR">Arkansas</option>
+              <option value="CA">California</option>
+              <option value="CO">Colorado</option>
+              <option value="CT">Connecticut</option>
+              <option value="DE">Delaware</option>
+              <option value="DC">District Of Columbia</option>
+              <option value="FL">Florida</option>
+              <option value="GA">Georgia</option>
+              <option value="HI">Hawaii</option>
+              <option value="ID">Idaho</option>
+              <option value="IL">Illinois</option>
+              <option value="IN">Indiana</option>
+              <option value="IA">Iowa</option>
+              <option value="KS">Kansas</option>
+              <option value="KY">Kentucky</option>
+              <option value="LA">Louisiana</option>
+              <option value="ME">Maine</option>
+              <option value="MD">Maryland</option>
+              <option value="MA">Massachusetts</option>
+              <option value="MI">Michigan</option>
+              <option value="MN">Minnesota</option>
+              <option value="MS">Mississippi</option>
+              <option value="MO">Missouri</option>
+              <option value="MT">Montana</option>
+              <option value="NE">Nebraska</option>
+              <option value="NV">Nevada</option>
+              <option value="NH">New Hampshire</option>
+              <option value="NJ">New Jersey</option>
+              <option value="NM">New Mexico</option>
+              <option value="NY">New York</option>
+              <option value="NC">North Carolina</option>
+              <option value="ND">North Dakota</option>
+              <option value="OH">Ohio</option>
+              <option value="OK">Oklahoma</option>
+              <option value="OR">Oregon</option>
+              <option value="PA">Pennsylvania</option>
+              <option value="RI">Rhode Island</option>
+              <option value="SC">South Carolina</option>
+              <option value="SD">South Dakota</option>
+              <option value="TN">Tennessee</option>
+              <option value="TX">Texas</option>
+              <option value="UT">Utah</option>
+              <option value="VT">Vermont</option>
+              <option value="VA">Virginia</option>
+              <option value="WA">Washington</option>
+              <option value="WV">West Virginia</option>
+              <option value="WI">Wisconsin</option>
+              <option value="WY">Wyoming</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <h4>Social Media Links (Optional)</h4>
             <input
               type="facebook"
-              placeholder="Facebook Link (Optional)"
+              placeholder="Facebook Link "
               name="facebook"
               value={facebook}
               onChange={(e) => onChange(e)}
             />
-          </div><div className="form-group">
+          </div>
+          <div className="form-group">
             <input
               type="instagram"
-              placeholder="Instagram Link (Optional)"
+              placeholder="Instagram Link "
               name="instagram"
               value={instagram}
               onChange={(e) => onChange(e)}
             />
-          </div><div className="form-group">
+          </div>
+          <div className="form-group">
             <input
               type="twitter"
-              placeholder="Twitter Link (Optional)"
+              placeholder="Twitter Link "
               name="twitter"
               value={twitter}
               onChange={(e) => onChange(e)}
             />
           </div>
-          
-          <input type="submit" className="btn btn-outline-light" value="Create" />
+
+          <input
+            type="submit"
+            className="btn btn-outline-light"
+            value="Create"
+          />
         </form>
       </div>
     </Fragment>
-    )
-}
+  );
+};
 
-CreateProfile.propTypes = {
+CreateProfile.propTypes = {};
 
-}
-
-export default connect()(CreateProfile)
+export default connect()(CreateProfile);
