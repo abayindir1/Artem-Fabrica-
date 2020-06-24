fc27e4344b8b1088be603fcd0aff391b057e3f37
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-06-24 14:22:03 -0500

    draw text and draw data on component

--- client/src/components/Draw/Draw.js
+++ client/src/components/Draw/Draw.js
@@ -1,35 +1,59 @@
-import React, { Fragment, useRef} from "react";
+import React, { Fragment, useRef } from "react";
 import CanvasDraw from "react-canvas-draw";
 
 const Draw = (props) => {
-    const [formData, setFormData] = React.useState({
-        color: "",
-        brushRadious: 5
-      });
-      const { color, brushRadious } = formData;
+  const [formData, setFormData] = React.useState({
+    postText: "",
+    color: "",
+    brushRadious: 5,
+  });
+  const {postText,  color, brushRadious } = formData;
 
-      const canvasRef = useRef(null)
+  const canvasRef = useRef(null);
 
-      const onSave =() =>{
-            var data = canvasRef.current.getSaveData()
-            console.log(data)
-      }
+  const onSave = () => {
+    var DrawData = canvasRef.current.getSaveData();
+    console.log(postText)
+    console.log(DrawData)
+}
 
-      const onClear= () =>{
-          canvasRef.current.clear()
-      }
 
-      const onUndo= () =>{
-        canvasRef.current.undo()
-    }
+  const onClear = () => {
+    canvasRef.current.clear();
+  };
+
+  const onUndo = () => {
+    canvasRef.current.undo();
+  };
   return (
     <Fragment>
-      <CanvasDraw canvasWidth={400} canvasHeight={400}
-       style={{border: '3px solid red'}} ref={canvasRef}/>
+      <form>
+        <div className="form-group">
+          <input
+            type="postText"
+            placeholder="Tell something about this work"
+            name="postText"
+            value={postText}
+            onChange={(e)=>{setFormData({ ...formData, [e.target.name]: e.target.value });}}
+          ></input>
+        </div>
+      </form>
+      <CanvasDraw
+        canvasWidth={400}
+        canvasHeight={400}
+        style={{ border: "3px solid red" }}
+        ref={canvasRef}
+      />
 
-       <button className="btn btn-success" onClick={onSave}>Save</button>
-       <button className="btn btn-warning" onClick={onUndo}>Undo</button>
-       <button className="btn btn-danger" onClick={onClear}>Clear</button>
+      <button className="btn btn-success" onClick={onSave}>
+        Save
+      </button>
+      <button className="btn btn-warning" onClick={onUndo}>
+        Undo
+      </button>
+      <button className="btn btn-danger" onClick={onClear}>
+        Clear
+      </button>
     </Fragment>
   );
 };
