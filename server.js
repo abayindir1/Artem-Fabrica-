85294a9f2315edd156ab90b77a121e6714011f55
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-08-03 13:10:45 -0500

    heroku

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use("/api/auth", authRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/posts", postRouter)
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 
 mongoose.connection.on("open", ()=> {
