5af0b2e65c232f0fc3c48c19964a1beb9b0420e4
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-08-03 15:16:42 -0500

    heroku

--- server.js
+++ server.js
@@ -21,16 +21,15 @@ app.use("/api/auth", authRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/posts", postRouter)
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
-    });
-  }
-console.log(__dirname)
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+//     });
+//   }
 
 mongoose.connection.on("open", ()=> {
     console.log("Connection Made")
