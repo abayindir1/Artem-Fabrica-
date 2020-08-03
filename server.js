634c9720f65ee9186476ab8647a7b1947a1d9b0e
Abdullah Bayindir <byndrabdullah@gmail.com>
2020-08-03 15:21:37 -0500

    heroku

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const usersRouter = require("./routes/api/users")
 const config = require("config")
 const path = require('path');
 
-app = express();
-const PORT = process.env.PORT || 3001
 
 const db = config.get("mongoURI")
 mongoose.connect(db)
+
+app = express();
+const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -21,15 +22,15 @@ app.use("/api/auth", authRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/posts", postRouter)
 
-// // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
   
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
-//     });
-//   }
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+    });
+  }
 
 mongoose.connection.on("open", ()=> {
     console.log("Connection Made")
